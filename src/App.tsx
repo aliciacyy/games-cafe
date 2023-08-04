70c432f086d9813a0d1334cac01c56c5f2dfc5d2
aliciacyy <bananachurn@gmail.com>
2023-08-05 00:55:24 +0800

    add new games

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,9 @@ function App() {
         }
       })
     }
-    const options = {}
+    const options = {
+      rootMargin: '-50px',
+    }
     const myObserver = new IntersectionObserver(callback, options)
   
     const imgList = document.querySelectorAll(".leCards")
@@ -30,29 +32,32 @@ function App() {
   });
 
   const games = [
-    createGameObj('Bananagrams', '2 and above', 'bananagrams.jpg'),
+    createGameObj('Bananagrams', '1 to 8', 'bananagrams.jpg'),
     createGameObj('Catan', '3 to 4', 'catan.jpg'),
     createGameObj('Codenames', '2 and above', 'codenames.jpg'),
     createGameObj('Codenames Pictures', '2 and above', 'codenames_pictures.jpg'),
     createGameObj('Deep Sea Adventure', '1 to 4', 'deep_sea.jpg'),
-    createGameObj('Enter the Spudnet', '1 to 4', 'spudnet.jpg'),
-    createGameObj('Exploding Kittens', '1 to 4', 'exploding.jpg'),
-    createGameObj('F***k the Game', '1 to 4', 'fk.jpg'),
-    createGameObj('Hanabi', '1 to 4', 'hanabi.jpg'),
-    createGameObj('Love Letter', '1 to 4', 'love_letter.jpg'),
-    createGameObj('Organ Attack', '1 to 4', 'organ.jpg'),
-    createGameObj('Plantopia', '1 to 4', 'plantopia.jpg'),
-    createGameObj('Poetry for Neanderthals', '1 to 4', 'poetry.jpg'),
-    createGameObj('POOP the Game', '1 to 4', 'poop.jpg'),
-    createGameObj('Rummikub', '1 to 4', 'rummikub.jpg'),
-    createGameObj('Saboteur', '1 to 4', 'saboteur.jpg'),
-    createGameObj('Spot It', '1 to 4', 'spot_it.jpg'),
-    createGameObj('Sushi Go Party', '1 to 4', 'sushi.jpg'),
-    createGameObj('Taco Cat Goat Cheese Pizza', '2 and above', 'taco.jpg'),
-    createGameObj('Take 5', '1 to 4', 'take5.jpg'),
-    createGameObj('Teabbles', '1 to 4', 'teabbles.jpg'),
-    createGameObj('The Singaporean Dream', '1 to 4', 'poetry.jpg'),
-    createGameObj('Toxic People', '1 to 4', 'toxic.jpg'),
+    createGameObj('Dixit', '3 to 8', 'dixit.jpg'),
+    createGameObj('Enter the Spudnet', '2 to 6', 'spudnet.jpg'),
+    createGameObj('Exploding Kittens', '2 to 8', 'exploding.jpg'),
+    createGameObj('F***k the Game', '3 to 8', 'fk.jpg'),
+    createGameObj('Hanabi', '2 to 5', 'hanabi.jpg'),
+    createGameObj('Here to Slay', '2 to 6', 'poetry.jpg'),
+    createGameObj('Love Letter', '2 to 6', 'love_letter.jpg'),
+    createGameObj('Mysterium', '2 to 7', 'mysterium.jpg'),
+    createGameObj('Organ Attack', '2 to 6', 'organ.jpg'),
+    createGameObj('Plantopia', '2 to 5', 'plantopia.jpg'),
+    createGameObj('Poetry for Neanderthals', '2 and above', 'poetry.jpg'),
+    createGameObj('POOP the Game', '2 to 5', 'poop.jpg'),
+    createGameObj('Rummikub', '2 to 4', 'rummikub.jpg'),
+    createGameObj('Saboteur', '2 to 12', 'saboteur.jpg'),
+    createGameObj('Spot It', '2 to 8', 'spot_it.jpg'),
+    createGameObj('Sushi Go Party', '2 to 8', 'sushi.jpg'),
+    createGameObj('Taco Cat Goat Cheese Pizza', '2 to 8', 'taco.jpg'),
+    createGameObj('Take 5', '2 to 10', 'take5.jpg'),
+    createGameObj('Teabbles', '2 to 5', 'teabbles.jpg'),
+    createGameObj('The Singaporean Dream', '2 to 5', 'singaporean.jpg'),
+    createGameObj('Toxic People', '2 to 6', 'toxic.jpg'),
     createGameObj('Unstable Unicorns', '2 to 8', 'unstable.jpg'),
   ]
 
@@ -70,13 +75,18 @@ function App() {
   return (
     <div>
       <div className="container max-w-full bg-gray-200 p-8 h-[60vh] xl:h-[80vh] flex justify-center items-center">
-        <p className="leCards text-6xl xl:text-8xl text-gray-700 font-bold mb-8 text-center">
-          <span className='text-gray-900'>A</span>lisiang <span className='text-gray-900'>G</span>aming <span className='text-gray-900'>O</span>asis
-        </p>
+        <div>
+          <div className="leCards text-6xl xl:text-8xl text-gray-700 font-bold mb-8 text-center">
+            <span className='text-gray-900'>A</span>lisiang <span className='text-gray-900'>G</span>aming <span className='text-gray-900'>O</span>asis
+          </div>
+          <div className="leCards text-lg xl:text-3xl text-gray-500 mb-8 text-center">
+            By invitation only
+          </div>
+        </div>
       </div>
 
       <div className="container max-w-full bg-gray-300 p-8">
-        <div className="text-4xl xl:text-6xl text-gray-700 font-bold mb-8 text-center">Our Games</div>
+        <div className="text-4xl xl:text-6xl text-gray-700 font-bold mb-8 text-center">Games</div>
         <div className="flex gap-4 flex-wrap justify-center">
           {renderGames()}
         </div>
